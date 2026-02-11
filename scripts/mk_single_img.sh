aa8b6fcce0a15dd8c657763c409b727225e7d0b1
Jian Yang <pub@yangjian.net>
2026-02-11 07:02:49 -0800

    fix build.sh and get kernel version from source instead of hard-coded

--- scripts/mk_single_img.sh
+++ scripts/mk_single_img.sh
@@ -7,30 +7,44 @@ set -e
 
 MNTDIR=/mnt/rootfs
 ROOTFS_TGZ=${TOPDIR}/rootfs.tar.gz
+kernel=6.6.104
 
-IMG_FILE=${BOARD}-${service}-sd.img
+BOOT_MEDIA=${1:-''}
+BUILD_MODE=${2:-''}
+test -n "$BOOT_MEDIA" || select BOOT_MEDIA in sdmmc emmc
+do
+  test -n "$BOOT_MEDIA" && break
+done
+
+# update uEnv.txt and copy boot files
+sed -i -e "s/^fdt=.*/fdt=mt7988a-bananapi-bpi-r4-${BOOT_MEDIA}-snand.dtb/" $TOPDIR/mt-pack/mtk/bpi-r4/configs/default/${service}/uEnv.txt
+test -n "$BUILD_MODE" && ( cd $TOPDIR; ./build.sh $BUILD_MODE )
+
+
+IMG_FILE=${BOARD}-linux-${kernel}-${BOOT_MEDIA}.img
 rm -f $IMG_FILE
+
 
 # prepare image file
 truncate -s 8G $IMG_FILE
 LOOP_DEV=`sudo losetup -fP --show ${IMG_FILE}`
+trap "sudo umount -R $MNTDIR; sudo losetup -d $LOOP_DEV" TERM EXIT
 
 # drop-in bootloader/u-boot
 GPT=$TOPDIR/mt-pack/mtk/bpi-r4/bin/gpt
-BL2=$TOPDIR/atf-mt/build_sdmmc/mt7988/release/bl2.img
-FIP=$TOPDIR/u-boot-mt/build_sdmmc/u-boot_sdmmc.fip
+BL2=$TOPDIR/atf-mt/build_${BOOT_MEDIA}/mt7988/release/bl2.img
+FIP=$TOPDIR/u-boot-mt/build_${BOOT_MEDIA}/u-boot_${BOOT_MEDIA}.fip
 
 sudo dd if=$GPT	of=${LOOP_DEV} conv=fsync,notrunc
 sudo dd if=$BL2	of=${LOOP_DEV} conv=fsync,notrunc bs=512 seek=34
 sudo dd if=$FIP	of=${LOOP_DEV} conv=fsync,notrunc bs=512 seek=13312
 
-sudo partprobe $LOOP_DEV
+#sudo sgdisk -e $LOOP_DEV
+sudo partprobe $LOOP_DEV ||true
 
 # prepare file system
 sudo mkfs.fat -F 32 -n BPI-BOOT ${LOOP_DEV}p5
 sudo mkfs.ext4 -L BPI-ROOT ${LOOP_DEV}p6
-
-sudo losetup -d ${LOOP_DEV}
 
 # drop-in BPI-ROOT: rootfs and kernel modules
 sudo mount ${LOOP_DEV}p6 $MNTDIR
@@ -41,7 +55,6 @@ sudo rsync -rlptKHW $TOPDIR/SD/BPI-ROOT/ $MNTDIR/
 sudo mount ${LOOP_DEV}p5 $MNTDIR/boot
 sudo rsync -r $TOPDIR/SD/BPI-BOOT/* $MNTDIR/boot/
 
-read -p 'press ENTER to continue' x
-# clean
-sudo umount -R $MNTDIR
-sudo losetup -d $LOOP_DEV
+${DEBUG:-false} && read -p 'press ENTER to continue' x
+
+echo "Image $IMG_FILE created successfully"
